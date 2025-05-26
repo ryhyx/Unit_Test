cdae09087269593c6a9448cf9721e229430d28d7
r.shakiba <r.shakiba@dotin.ir>
2025-05-26 08:27:06 +0330

    initial push test in VDI

--- AccountingPractice/Domain/Account.cs
+++ AccountingPractice/Domain/Account.cs
@@ -1,7 +1,25 @@
-﻿public abstract class Account{    public string AccountName { get; private set; }    private List<Article> _articles;    public Account(string accountName)    {        AccountName = accountName;        _articles = new List<Article>();    }
+﻿public abstract class Account
+{
+    public string AccountName { get; private set; }
+    private List<Article> _articles;
+
+    public Account(string accountName)
+    {
+        AccountName = accountName;
+        _articles = new List<Article>();
+    }
 
     // Adds an article to the account's list of articles
-    public void AddArticle(Article article)    {        _articles.Add(article);    }
+    public void AddArticle(Article article)
+    {
+        _articles.Add(article);
+    }
 
     // Provides access to the articles (used for balance calculations)
-    protected List<Article> GetArticles()    {        return _articles;    }    public abstract decimal CalculateBalance();}
+    protected List<Article> GetArticles()
+    {
+        return _articles;
+    }
+
+    public abstract decimal CalculateBalance();
+}
